c6eaed1803571e6548e78c583b8c880a05e2ad5b
Michael Hurley <Michael.Hurley@abcgrp.com>
2018-11-07 11:43:51 -0500

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default () => {
+    return [
+        { title: 'Harry Potter' },
+        { title: 'Sphere' },
+        { title: 'The 48 Laws of Power' },
+        { title: 'Extreme Ownership' },
+        { title: 'Of Mice and Men' },
+    ];
+}
